ca3dc22bc2a1944c6e02be09d3684f44e43337c3
Ricardo Camarena <ricky_camarena@hotmail.com>
2023-05-05 15:47:18 -0600

    feat: Keyboard detects if user pressed the return button. The search field alerts these if they leave it empty and try to search something. Keyboard gets dismissed if the user press the search button or the return key.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self //To assign UITextFieldDelegate to the searchTextField
+        
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) // To dismiss the keyboard
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool { // Detects if the user pressed the return button in the keyboard
+        searchTextField.endEditing(true) // To dismiss the keyboard
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something here" // Alert the user if they havn't typed anything in the search field
+            return false
+        }
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = "" // To empty the search field once the search button or the return key are pressed
+    }
+    
 }
 
